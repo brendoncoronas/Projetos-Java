9f1e2bae4f450493341f6194818f499943b0feae
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-12-11 11:19:48 -0300

    Add files via upload

---
+++ projeto005_2_estrutura_sequencial.java
@@ -0,0 +1,20 @@
+package projeto005_2_estrutura_sequencial;
+
+import java.util.Scanner;
+public class projeto005_2_estrutura_sequencial {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int soma = sc.nextInt();
+		int outroNumero = sc.nextInt();
+		
+		int resultado = soma + outroNumero;
+		
+		System.out.println(resultado);
+		
+
+	}
+
+}
