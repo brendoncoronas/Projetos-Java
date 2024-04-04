f97ed4a616a53ccce50e3a9996a7aaa109b407aa
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2024-04-04 11:33:32 -0300

    Add files via upload

---
+++ Computador.java
@@ -0,0 +1,9 @@
+package exercicios_p_iniciantes_estrutura_condicional;
+
+public class Computador {
+	
+	private String marca;
+	private int anoFabricacao;
+	private double velocidade;
+	private boolean novo;
+}
